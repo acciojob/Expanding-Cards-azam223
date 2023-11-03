b35cd5c1e9293e263cfa5ddbd13ee0cd4d3adff9
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 19:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function expandCard(panelId) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+        if (panel.id === panelId) {
+            panel.classList.toggle('active');
+            panel.classList.toggle('expanding');
+        } else {
+            panel.classList.remove('active');
+            panel.classList.remove('expanding');
+        }
+    });
+}
